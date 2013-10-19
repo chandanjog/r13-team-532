07d352b5f06d13f71ee7a23786b0a4d033d72e57
chandan jog <jog.chandan@gmail.com>
2013-10-19 11:17:06 +0530

    basic repo

---
+++ TouchTypeIn/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TouchTypeIn::Application.config.secret_key_base = 'f4f86faffbb430efb85a2cc03aada720a389ff5e8ff5cdffb8e37b52af78a59631be2d9339f387118566a25cc1d1a64cff4cf5f0ff20b785d562b52361e93c65'
